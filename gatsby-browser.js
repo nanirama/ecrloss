3d60627b18fce48c5c24f9b25f6dfffbd1b802d9
nanirama <nani.rama1982@gmail.com>
2021-12-15 11:21:53 +0530

    changes 39

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Layout from "./src/components/Layout";
 
 
 export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
+  return <div {...props}>{element}</div>;
 };
