821bb037ea8ae50b94a39d4c412361598984235a
nanirama <nani.rama1982@gmail.com>
2022-01-03 20:15:02 +0530

    research listing page 2

--- src/templates/EventFuturePastListTemplate.js
+++ src/templates/EventFuturePastListTemplate.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 
-import Paginate from '../components/blog/Paginate'
+import Paginate from '../components/Blog/Paginate'
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import { EventIndex } from '../components/Event';
